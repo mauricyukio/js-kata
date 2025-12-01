1b57bc6cd6221d7525c34e890dfedffbe8858e9e
Yukio Nagashima <109549228+mauricyukio@users.noreply.github.com>
2025-12-01 19:18:48 +0000

    Started writing main functions

--- 4kyu/skyscraper/skyscraper.js
+++ 4kyu/skyscraper/skyscraper.js
@@ -1,6 +1,27 @@
+const tableSize = 4
+const exampleMatrix = [[1,2,3,4], [2,3,4,1], [3,4,1,2], [4,1,2,3]]
+
 function compareTwoMatrixes(matrix1, matrix2)
 {
     console.log('compared matrixes');
     return;
 }
-compareTwoMatrixes;
+
+function transposeMatrix(matrix)
+{
+    const transposedMatrix = [];
+
+    console.log(exampleMatrix);
+    console.log(transposedMatrix);
+    return;
+}
+
+function eliminateCandidate(cell, candidate)
+{
+    console.log('candidates eliminated');
+    return;
+}
+
+compareTwoMatrixes();
+transposeMatrix(exampleMatrix);
+eliminateCandidate();
