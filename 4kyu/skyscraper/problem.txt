1771cbdab14a8d05ea5142c64ac4beab7314ff1c
Yukio Nagashima <109549228+mauricyukio@users.noreply.github.com>
2025-12-01 15:57:39 -0300

    Add files via upload
    Created initial files

---
+++ 4kyu/skyscraper/problem.txt
@@ -0,0 +1,1 @@
+https://www.codewars.com/kata/5671d975d81d6c1c87000022/train/javascript
---
+++ 4kyu/skyscraper/skyscraper.js
@@ -0,0 +1,6 @@
+function compareTwoMatrixes(matrix1, matrix2)
+{
+    console.log('compared matrixes');
+    return;
+}
+compareTwoMatrixes;
