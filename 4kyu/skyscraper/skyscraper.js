45aaf862b8d9bb297a2bb0162365b242136e4ca2
Yukio Nagashima <myynagashima@gmail.com>
2025-12-02 18:08:57 -0300

    Co-authored-by: Marcilio Nagashima <nagashimayudi@users.noreply.github.com>

--- 4kyu/skyscraper/skyscraper.js
+++ 4kyu/skyscraper/skyscraper.js
@@ -1,35 +1,104 @@
-const tableSize = 4
+const boardSize = 4;
 const exampleMatrix = [
     [2,1,4,3], 
     [3,4,1,2], 
     [4,2,3,1], 
     [1,3,2,4]];
 
-function compareTwoMatrixes(matrix1, matrix2) {
-    for (let i = 0; i < tableSize; i++) {
-        for (let j = 0; j < tableSize; j++) {
+// compareTwoMatrices(exampleMatrix, transposedMatrix, boardSize);
+
+const initialMatrix = generateInitialMatrix(boardSize);
+
+exampleCell = [4, 3, 2, 1];
+
+function generateInitialMatrix(size) {
+    let candidates = [];
+    for (let i = null; i < size; i++) {
+        candidates.push(i+1);
+    }
+
+    const line = new Array(size).fill(candidates);
+    const matrix = new Array(size).fill(line);
+    return matrix;
+}
+
+function compareTwoMatrices(matrix1, matrix2, size) {
+    for (let i = 0; i < size; i++) {
+        for (let j = 0; j < size; j++) {
             matrix1[i][j] === matrix2[i][j] ? console.log('equal') : console.log('different');
         }
     }
     return;
 }
 
-function transposeMatrix(matrix) {
-    const newArray = [];
-    for (let i = 0; i < tableSize; i++) {
+function transposeMatrix(matrix, size) {
+    const transposedArray = [];
+    for (let i = 0; i < size; i++) {
         const column = [];
-        for (let j = 0; j < tableSize; j++) { 
+        for (let j = 0; j < size; j++) { 
             column.push(matrix[j][i]);            
         }
-        newArray.push(column);
+        transposedArray.push(column);
     }
-    return newArray;
+    return transposedArray;
 }
 
 function eliminateCandidate(cell, candidate) {
-    console.log('candidates eliminated');
-    return;
+    cell[candidate - 1] = null;
 }
 
-console.log(transposeMatrix(exampleMatrix));
-console.log(compareTwoMatrixes(exampleMatrix, transposeMatrix(exampleMatrix)));
+function analyzeCell(xPosition, yPosition) {
+    const row = initialMatrix[xPosition];
+
+    const column = transposeMatrix(initialMatrix, boardSize)[yPosition];
+
+    const toEliminate = [];
+    
+    for (cell of row) {
+        console.log(typeof cell);
+        if (typeof cell === "number") {
+            toEliminate.push(cell);
+        }
+    }
+    for (cell of column) {
+        console.log(typeof cell);
+        if (typeof cell === "number") {
+            toEliminate.push(cell);
+        }
+    }
+
+    for (element of toEliminate) {
+        eliminateCandidate(initialMatrix[xPosition][yPosition], element);
+    }
+}
+
+function scanLine(line) {
+    console.log('scanned');
+}
+
+function fillCell(cell) {
+    let answer = null;
+    for (value of cell) {
+        if (value !== null) {
+            if (answer !== null) {
+                return cell;
+            } else {
+                answer = value;
+            }
+        }
+    }
+    return answer;
+}
+
+// console.log(initialMatrix);
+// initialMatrix[0][0] = 1;
+// initialMatrix[1][1] = 2;
+// initialMatrix[2][2] = 3;
+// initialMatrix[3][3] = 4;
+// console.log(initialMatrix);
+
+// // analyzeCell(0,3);
+
+// console.log(initialMatrix);
+
+eliminateCandidate(initialMatrix)
